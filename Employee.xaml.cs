e59d25af71bff0a831d2f4baa28eca05fa8d4fc8
JFer-B <bohorquez.julianf@gmail.com>
2020-09-04 20:16:57 -0500

    Punto 3: Desarrollo de Eventos

--- Employee.xaml.cs
+++ Employee.xaml.cs
@@ -28,5 +28,60 @@ namespace ParcialPractico1_JulianBohorquez
             MainWindow w = (MainWindow)Window.GetWindow(this);
             w.frameMain.NavigationService.Navigate(new LogIn());
         }
+        int flag = 0;
+        int Total = 0;
+        private void btnNewProduct_Click(object sender, RoutedEventArgs e)
+        {
+            string ClientName = txtClientName.Text;
+            string ClientCC = txtCC.Text;
+            string Product = txtProducto.Text;
+            string Cantidad = txtCantidad.Text;
+            int Subtotal = 0;
+            if(Product=="Pan Aliñado")
+            {
+                Subtotal = 1000 * int.Parse(Cantidad);
+            }
+            else
+            {
+                if(Product=="Pan No Aliñado")
+                {
+                    Subtotal = 500 * int.Parse(Cantidad);
+                }
+                else
+                {
+                    if(Product=="Pan Especial")
+                    {
+                        Subtotal = 2000 * int.Parse(Cantidad);
+                    }
+                }
+            }
+            Total = Total + Subtotal;
+            if(flag==0)
+            {
+                txtRegister.Text = txtRegister.Text + ClientName + "\n" + "C.C. " + ClientCC + "\n\n" + Product + " X " + Cantidad + ".........." + Subtotal.ToString();
+                txtCantidad.Clear();
+                flag = 1;
+            }
+            else
+            {
+                txtRegister.Text = txtRegister.Text + "\n" + Product + " X " + Cantidad + ".........." + Subtotal.ToString();
+                txtCantidad.Clear();
+            }
+        }
+
+        private void btnSell_Click(object sender, RoutedEventArgs e)
+        {
+            txtRegister.Text = txtRegister.Text + "\n\n" + "Total.........." + Total.ToString();
+        }
+
+        private void btnNewSell_Click(object sender, RoutedEventArgs e)
+        {
+            Total = 0;
+            flag = 0;
+            txtClientName.Clear();
+            txtCC.Clear();
+            txtCantidad.Clear();
+            txtRegister.Text = "";
+        }
     }
 }
